867fa20bc741f13a1264beac32b3722cb3f03254
Sol Astrius (Daniel Gorlov) <me@danielsol.dev>
2026-01-09 16:41:50 +0100

    Add Nix flake for packaging
    - flake.nix with rustPlatform.buildRustPackage
    - Cachix integration (solastrius cache)
    - GitHub Actions workflow for CI and cache push
    - Track Cargo.lock for reproducible builds

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "KOReader sync server with extended annotation support";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  nixConfig = {
+    extra-substituters = [ "https://solastrius.cachix.org" ];
+    extra-trusted-public-keys = [ "solastrius.cachix.org-1:MawFli42h9VuWjlURZvxDG+M/tfUbELRwU+QN/6VvdM=" ];
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "kosync-server";
+          version = "0.1.0";
+
+          src = ./server;
+
+          cargoLock = {
+            lockFile = ./server/Cargo.lock;
+          };
+
+          meta = with pkgs.lib; {
+            description = "KOReader sync server with extended annotation support";
+            homepage = "https://github.com/SolAstrius/kosync-rs";
+            license = licenses.agpl3Only;
+            maintainers = [ ];
+            mainProgram = "kosync-server";
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.default ];
+
+          packages = with pkgs; [
+            cargo
+            rustc
+            rust-analyzer
+            clippy
+            rustfmt
+          ];
+        };
+      }
+    );
+}
